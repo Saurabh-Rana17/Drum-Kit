37513f63a069a35d275edf497aecba2917224057
Saurabh Rana <saurabhrana200317@gmail.com>
2023-07-16 01:12:49 +0530

    js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function eventHandling() {
     alert("Got clicked");
 }
-for (var i = 0; i < 7; i++) {
-    document.querySelector("button")[i].addEventListener("click", eventHandling);
+for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
+    document.querySelectorAll("button")[i].addEventListener("click", eventHandling);
 }
