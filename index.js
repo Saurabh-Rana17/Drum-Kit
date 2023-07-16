ab91eafab2b52b297de6828e4f8858b582962e40
Saurabh Rana <saurabhrana200317@gmail.com>
2023-07-16 15:58:06 +0530

    kick bass error solved

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
                 audio.play();
                 break;
             case "l":
-                var audio = new Audio("sounds/kick.mp3");
+                var audio = new Audio("sounds/kick-bass.mp3");
                 audio.play();
                 break;
         
