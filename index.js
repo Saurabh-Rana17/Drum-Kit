a0f605376cf6c422159bd333869a78a5d10374f4
Saurabh Rana <saurabhrana200317@gmail.com>
2023-07-16 15:48:26 +0530

    switch

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-// function eventHandling() {
-//     alert("Got clicked");
-// }
-// for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
-//     document.querySelectorAll ("button")[i].addEventListener("click", eventHandling);
-// }
+
+for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
+    document.querySelectorAll("button")[i].addEventListener("click", function () {
+        var buttonInnerHTML = this.innerHTML;
+        switch (buttonInnerHTML) {
+            case "w":
+                var audio = new Audio("sounds/tom-1.mp3");
+                audio.play();
+                break;
+            
+        
+            default:
+                break;
+        }
+    });
+    
+}
+
+
+
+
+
+
 var tom1 = new Audio("sounds/tom-1.mp3");
-document.querySelector(".w").addEventListener("click", playTom1);
+// document.querySelector(".w").addEventListener("click", playTom1);
 function playTom1() {
     tom1.play();
+    console.log(this.textContent);
+    this.style.color = "blue";
 }
