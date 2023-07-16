56a494f5162c226665a3e884fe179c5debb3798f
Saurabh Rana <saurabhrana200317@gmail.com>
2023-07-16 16:32:49 +0530

    keypress

--- index.js
+++ index.js
@@ -38,16 +38,11 @@ for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
     });
     
 }
+document.addEventListener("keypress", function () {
+    alert("Key was Pressed");
+ })
 
 
 
 
 
-
-var tom1 = new Audio("sounds/tom-1.mp3");
-// document.querySelector(".w").addEventListener("click", playTom1);
-function playTom1() {
-    tom1.play();
-    console.log(this.textContent);
-    this.style.color = "blue";
-}
