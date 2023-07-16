be769643f16807a34269587f73618b27edaa750d
Saurabh Rana <saurabhrana200317@gmail.com>
2023-07-16 13:28:16 +0530

    audio added

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-function eventHandling() {
-    alert("Got clicked");
-}
-for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
-    document.querySelectorAll("button")[i].addEventListener("click", eventHandling);
+// function eventHandling() {
+//     alert("Got clicked");
+// }
+// for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
+//     document.querySelectorAll ("button")[i].addEventListener("click", eventHandling);
+// }
+var tom1 = new Audio("sounds/tom-1.mp3");
+document.querySelector(".w").addEventListener("click", playTom1);
+function playTom1() {
+    tom1.play();
 }
