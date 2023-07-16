f18fbc12e9a56aa3a33680064cc88f1a79c4c6f5
Saurabh Rana <saurabhrana200317@gmail.com>
2023-07-16 16:27:25 +0530

    aplha chnage

--- index.js
+++ index.js
@@ -3,19 +3,19 @@ for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
     document.querySelectorAll("button")[i].addEventListener("click", function () {
         var buttonInnerHTML = this.innerHTML;
         switch (buttonInnerHTML) {
-            case "w":
+            case "q":
                 var audio = new Audio("sounds/tom-1.mp3");
                 audio.play();
                 break;
-            case "a":
+            case "w":
                 var audio = new Audio("sounds/tom-2.mp3");
                 audio.play();
                 break;
-            case "s":
+            case "e":
                 var audio = new Audio("sounds/tom-3.mp3");
                 audio.play();
                 break;
-            case "d":
+            case "r":
                 var audio = new Audio("sounds/tom-4.mp3");
                 audio.play();
                 break;
